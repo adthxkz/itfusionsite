ea54b158af0d9f34a5d22b6ff4faa940d0543952
Адиль <one.akk.steam@gmail.com>
2025-07-24 23:35:00 +0500

    Исправлена страница thank-you.php: добавлены header/footer шаблоны

--- thank-you.php
+++ thank-you.php
@@ -1,21 +1,10 @@
 <?php
     $pageTitle = "Спасибо за вашу заявку! - IT Fusion";
     $pageDescription = "Ваша заявка принята. Скоро мы с вами свяжемся.";
-    $pageName = "thank-you";?>
-
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <title><?php echo $pageTitle; ?></title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="<?php echo $pageDescription; ?>">
-    <meta name="robots" content="noindex, nofollow">
-    <link rel="stylesheet" href="/css/custom.css">
-    <link rel="icon" href="/img/logo_it.png">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-</head>
-<body>
+    $pageName = "thank-you";
+    
+    include 'header.php';
+?>
 
 <main>
     <section class="section text-center">
@@ -28,6 +17,9 @@
             <a href="/index.php" class="button button-secondary" style="margin-top: 20px;">Вернуться на главную</a>
         </div>
     </section>
+</main>
+
+<?php include 'footer.php'; ?>
 </main>
 
 
